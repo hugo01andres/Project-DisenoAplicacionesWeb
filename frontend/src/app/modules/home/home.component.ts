066e42f461fcb5b72bdb31e9e96d03318c4dd6eb
hugo01andres <hugoandres01@live.com.mx>
2024-02-13 20:37:25 -0600

    Frontend

---
+++ frontend/src/app/modules/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../../shared/menu/menu/menu.component';
+import { TaskListComponent } from '../tarea/tarea-list/task-list.component';
+import { TaskService } from '../tarea/task.service';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuComponent, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  user = '';
+
+  constructor(private _taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.getTaskList();
+  }
+
+  getTaskList() {
+    this._taskService.getTaskList().subscribe(
+      (data) => {
+        console.log(data);
+        this.user = data[0].user.username;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+}
